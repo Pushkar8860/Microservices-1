1fd6cd5a200b6acdcd947563d9d094f9165825ad
Pushkar <pushkar@nagarro.com>
2022-11-28 18:48:16 +0530

    commit

--- src/main/java/com/tutorials/user/service/usersercice/UserserciceApplication.java
+++ src/main/java/com/tutorials/user/service/usersercice/UserserciceApplication.java
@@ -2,10 +2,11 @@ package com.tutorials.user.service.usersercice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserserciceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserserciceApplication.class, args);
 	}
